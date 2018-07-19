baa12a9d62997f17c53cf22c6e5b9c7ad8623cc3
jenlcabral <jen.cabral@Norimaconsulting.com>
2018-07-19 12:33:46 -0500

    First attempt - does not work on subfolders

--- FtpHelper/Program.cs
+++ FtpHelper/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
 
 namespace FtpHelper
 {
@@ -6,7 +8,52 @@ namespace FtpHelper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FileSystemWatcher fileSystemWatcher = new FileSystemWatcher(@"C:\Temp\ftpfolder");
+            fileSystemWatcher.IncludeSubdirectories = true;
+            fileSystemWatcher.Created += new FileSystemEventHandler(OnReceiveFiles);
+            fileSystemWatcher.EnableRaisingEvents = true;
+            Console.WriteLine("Restarting IIS");
+            Console.ReadLine();
+
+
+        }
+
+        public static void OnReceiveFiles(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Found file " + e.FullPath);
+            string destinationPath = @"C:\Temp\movedToFolder";
+            FileInfo fileToMove = new FileInfo(e.FullPath);
+            string fullDestinationPath = destinationPath + "\\"+ fileToMove.Name;
+            WaitForFinishWrite(e.FullPath, fullDestinationPath);
+
+            Console.WriteLine("File done writing " + e.FullPath + " move to " + fullDestinationPath);
+            if (!Directory.Exists(destinationPath))
+            {
+                Directory.CreateDirectory(destinationPath);
+            }
+            if (File.Exists(fullDestinationPath))
+                File.Delete(fullDestinationPath);
+            File.Move(e.FullPath, fullDestinationPath);
+        }
+        private static void WaitForFinishWrite(string sourcePath, string destinationPath)
+        {
+            while (true)
+            {
+                try {
+                    using (StreamReader stream = new StreamReader(sourcePath))
+                    {
+
+                        
+
+                        break;
+                    }
+
+                }
+                catch
+                {
+                    Thread.Sleep(1000);
+                }
+            }
         }
     }
 }
