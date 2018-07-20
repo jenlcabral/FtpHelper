b0bba776ddee49c9fdc2f9757cb1da2b57966dbb
jenlcabral <jen.cabral@Norimaconsulting.com>
2018-07-20 09:32:10 -0500

    Removing watching and only adding check for new files

--- FtpHelper/Program.cs
+++ FtpHelper/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 
 namespace FtpHelper
@@ -8,32 +10,44 @@ namespace FtpHelper
     {
         static void Main(string[] args)
         {
-            FileSystemWatcher fileSystemWatcher = new FileSystemWatcher(@"C:\Temp\ftpfolder");
-            fileSystemWatcher.IncludeSubdirectories = true;
-            fileSystemWatcher.Created += new FileSystemEventHandler(OnReceiveFiles);
-            fileSystemWatcher.EnableRaisingEvents = true;
-            Console.WriteLine("Restarting IIS");
+
+            // check if files are in directory. 
+            // if files found,
+            //// if not, turn off app
+            string path = @"C:\Temp\ftpfolder";
+            if (Directory.EnumerateFileSystemEntries(path).Any())
+            {
+                Console.WriteLine("there are files to transfer");
+                Console.WriteLine("turning off IIS");
+                Console.WriteLine("moving files");
+                //turn off IIS - transfer files to iis location, turn IIS back on after transfer is done
+                IEnumerable<string> files = Directory.EnumerateFiles(path,"*", SearchOption.AllDirectories);
+                foreach(string file in files)
+                {
+                    OnReceiveFiles(file, path);
+                }
+            }
             Console.ReadLine();
-
-
         }
 
-        public static void OnReceiveFiles(object sender, FileSystemEventArgs e)
+        public static void OnReceiveFiles(string filePathMove, string currentPath)
         {
-            Console.WriteLine("Found file " + e.FullPath);
+            Console.WriteLine("Found file " + filePathMove);
             string destinationPath = @"C:\Temp\movedToFolder";
-            FileInfo fileToMove = new FileInfo(e.FullPath);
-            string fullDestinationPath = destinationPath + "\\"+ fileToMove.Name;
-            WaitForFinishWrite(e.FullPath, fullDestinationPath);
+            FileInfo fileToMove = new FileInfo(filePathMove);
+            string dirToAdd = fileToMove.Directory.ToString().Remove(0, currentPath.Length);
+            destinationPath = destinationPath + dirToAdd;
+            string fullDestinationPath = destinationPath + "\\" + fileToMove.Name;
+            WaitForFinishWrite(filePathMove, fullDestinationPath);
 
-            Console.WriteLine("File done writing " + e.FullPath + " move to " + fullDestinationPath);
+            Console.WriteLine("File done writing " + filePathMove + " move to " + fullDestinationPath);
             if (!Directory.Exists(destinationPath))
             {
                 Directory.CreateDirectory(destinationPath);
             }
             if (File.Exists(fullDestinationPath))
                 File.Delete(fullDestinationPath);
-            File.Move(e.FullPath, fullDestinationPath);
+            File.Move(filePathMove, fullDestinationPath);
         }
         private static void WaitForFinishWrite(string sourcePath, string destinationPath)
         {
