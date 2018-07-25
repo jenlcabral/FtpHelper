2cb80da686c7e5f092b63dac6a396520930458ce
jenlcabral <jen.cabral@Norimaconsulting.com>
2018-07-25 11:00:28 -0500

    removing unnecessary logging stuff

--- FtpHelper.Logging/LoggingExtensions.cs
+++ FtpHelper.Logging/LoggingExtensions.cs
@@ -21,139 +21,139 @@ namespace FtpHelper.Logging
 
         #region Performance
 
-        public static bool IsPerformanceEnabled(this ILog log)
-        {
-            return log.Logger.IsEnabledFor(CustomLogLevels.PerformanceLevel);
-        }
+        //public static bool IsPerformanceEnabled(this ILog log)
+        //{
+        //    return log.Logger.IsEnabledFor(CustomLogLevels.PerformanceLevel);
+        //}
 
-        public static void Performance(this ILog log, string message)
-        {
-            CallLogger(log, CustomLogLevels.PerformanceLevel, message, null);
-        }
+        //public static void Performance(this ILog log, string message)
+        //{
+        //    CallLogger(log, CustomLogLevels.PerformanceLevel, message, null);
+        //}
 
-        public static void PerformanceFormat(this ILog log, string message, params object[] args)
-        {
-            CallLogger(log, CustomLogLevels.PerformanceLevel, message, null, args);
-        }
+        //public static void PerformanceFormat(this ILog log, string message, params object[] args)
+        //{
+        //    CallLogger(log, CustomLogLevels.PerformanceLevel, message, null, args);
+        //}
 
         #endregion
 
         #region Sql
 
-        public static bool IsSqlEnabled(this ILog log)
-        {
-            return log.Logger.IsEnabledFor(CustomLogLevels.SqlLevel);
-        }
+        //public static bool IsSqlEnabled(this ILog log)
+        //{
+        //    return log.Logger.IsEnabledFor(CustomLogLevels.SqlLevel);
+        //}
 
-        public static void Sql(this ILog log, string message)
-        {
-            CallLogger(log, CustomLogLevels.SqlLevel, message, null);
-        }
+        //public static void Sql(this ILog log, string message)
+        //{
+        //    CallLogger(log, CustomLogLevels.SqlLevel, message, null);
+        //}
 
-        public static void SqlFormat(this ILog log, string message, params object[] args)
-        {
-            CallLogger(log, CustomLogLevels.SqlLevel, message, null, args);
-        }
+        //public static void SqlFormat(this ILog log, string message, params object[] args)
+        //{
+        //    CallLogger(log, CustomLogLevels.SqlLevel, message, null, args);
+        //}
 
         #endregion
 
         #region Communication
 
-        public static bool IsCommunicationEnabled(this ILog log)
-        {
-            return log.Logger.IsEnabledFor(CustomLogLevels.CommunicationLevel);
-        }
+        //public static bool IsCommunicationEnabled(this ILog log)
+        //{
+        //    return log.Logger.IsEnabledFor(CustomLogLevels.CommunicationLevel);
+        //}
 
-        public static void Communication(this ILog log, string message)
-        {
-            CallLogger(log, CustomLogLevels.CommunicationLevel, message, null);
-        }
+        //public static void Communication(this ILog log, string message)
+        //{
+        //    CallLogger(log, CustomLogLevels.CommunicationLevel, message, null);
+        //}
 
-        public static void CommunicationFormat(this ILog log, string message, params object[] args)
-        {
-            CallLogger(log, CustomLogLevels.CommunicationLevel, message, null, args);
-        }
+        //public static void CommunicationFormat(this ILog log, string message, params object[] args)
+        //{
+        //    CallLogger(log, CustomLogLevels.CommunicationLevel, message, null, args);
+        //}
 
         #endregion
 
         #region CommandExecution
 
-        public static bool IsCommandExecutionEnabled(this ILog log)
-        {
-            return log.Logger.IsEnabledFor(CustomLogLevels.CommandExecutionLevel);
-        }
+        //public static bool IsCommandExecutionEnabled(this ILog log)
+        //{
+        //    return log.Logger.IsEnabledFor(CustomLogLevels.CommandExecutionLevel);
+        //}
 
-        public static void CommandExecution(this ILog log, string message)
-        {
-            CallLogger(log, CustomLogLevels.CommandExecutionLevel, message, null);
-        }
+        //public static void CommandExecution(this ILog log, string message)
+        //{
+        //    CallLogger(log, CustomLogLevels.CommandExecutionLevel, message, null);
+        //}
 
-        public static void CommandExecutionFormat(this ILog log, string message, params object[] args)
-        {
-            CallLogger(log, CustomLogLevels.CommandExecutionLevel, message, null, args);
-        }
+        //public static void CommandExecutionFormat(this ILog log, string message, params object[] args)
+        //{
+        //    CallLogger(log, CustomLogLevels.CommandExecutionLevel, message, null, args);
+        //}
 
         #endregion
 
         #region Audit
 
-        public static bool IsAuditEnabled(this ILog log)
-        {
-            return log.Logger.IsEnabledFor(CustomLogLevels.AuditLevel);
-        }
+        //public static bool IsAuditEnabled(this ILog log)
+        //{
+        //    return log.Logger.IsEnabledFor(CustomLogLevels.AuditLevel);
+        //}
 
-        public static void Audit(this ILog log, string message)
-        {
-            CallLogger(log, CustomLogLevels.AuditLevel, message, null);
-        }
+        //public static void Audit(this ILog log, string message)
+        //{
+        //    CallLogger(log, CustomLogLevels.AuditLevel, message, null);
+        //}
 
-        public static void AuditFormat(this ILog log, string message, params object[] args)
-        {
-            CallLogger(log, CustomLogLevels.AuditLevel, message, null, args);
-        }
+        //public static void AuditFormat(this ILog log, string message, params object[] args)
+        //{
+        //    CallLogger(log, CustomLogLevels.AuditLevel, message, null, args);
+        //}
 
         #endregion
 
         #region SupportRequest
 
-        public static bool IsSupportRequestEnabled(this ILog log)
-        {
-            return log.Logger.IsEnabledFor(CustomLogLevels.SupportRequestLevel);
-        }
+        //public static bool IsSupportRequestEnabled(this ILog log)
+        //{
+        //    return log.Logger.IsEnabledFor(CustomLogLevels.SupportRequestLevel);
+        //}
 
-        public static void SupportRequest(this ILog log, string message)
-        {
-            CallLogger(log, CustomLogLevels.SupportRequestLevel, message, null);
-        }
+        //public static void SupportRequest(this ILog log, string message)
+        //{
+        //    CallLogger(log, CustomLogLevels.SupportRequestLevel, message, null);
+        //}
 
-        public static void SupportRequest(this ILog log, string message, Exception exception)
-        {
-            CallLogger(log, CustomLogLevels.SupportRequestLevel, message, exception);
-        }
+        //public static void SupportRequest(this ILog log, string message, Exception exception)
+        //{
+        //    CallLogger(log, CustomLogLevels.SupportRequestLevel, message, exception);
+        //}
 
-        public static void SupportRequestFormat(this ILog log, string message, params object[] args)
-        {
-            CallLogger(log, CustomLogLevels.SupportRequestLevel, message, null, args);
-        }
+        //public static void SupportRequestFormat(this ILog log, string message, params object[] args)
+        //{
+        //    CallLogger(log, CustomLogLevels.SupportRequestLevel, message, null, args);
+        //}
 
         #endregion
 
         #region Trace
 
-        public static bool IsTraceEnabled(this ILog log)
-        {
-            return log.Logger.IsEnabledFor(Level.Trace);
-        }
+        //public static bool IsTraceEnabled(this ILog log)
+        //{
+        //    return log.Logger.IsEnabledFor(Level.Trace);
+        //}
 
-        public static void Trace(this ILog log, string message)
-        {
-            CallLogger(log, Level.Trace, message, null);
-        }
+        //public static void Trace(this ILog log, string message)
+        //{
+        //    CallLogger(log, Level.Trace, message, null);
+        //}
 
-        public static void TraceFormat(this ILog log, string message, params object[] args)
-        {
-            CallLogger(log, Level.Trace, message, null, args);
-        }
+        //public static void TraceFormat(this ILog log, string message, params object[] args)
+        //{
+        //    CallLogger(log, Level.Trace, message, null, args);
+        //}
 
         #endregion
 
